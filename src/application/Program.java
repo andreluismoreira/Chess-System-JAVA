f57808bc91d2cb1e042af64228dc94ec0fe090c7
andre luis <andreluistma@gmail.com>
2021-04-29 15:48:56 -0300

    Criação do tabuleiro e inicio da mecanica do jogo

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,13 @@
 package application;
 
+import chess.ChessMatch;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 
+		ChessMatch chessMatch = new ChessMatch();
+		 Ui.printBoard(chessMatch.getPieces());	 	
 	}
-
 }
---
+++ src/boardGame/Piece.java
@@ -0,0 +1,18 @@
+package boardGame;
+
+public class Piece {
+
+	protected Position position;
+	private Board board;
+	
+	public Piece( Board board) {
+		super();
+		this.board = board;
+	}
+
+	protected Board getBoard() {
+		return board;
+	}
+
+	
+}
